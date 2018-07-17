ee98af4314196229d907676eaca1a227d0bf6f2b
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2018-07-17 02:54:38 +0100

    minor

--- sales/resources/views/welcome.blade.php
+++ sales/resources/views/welcome.blade.php
@@ -369,10 +369,6 @@
         </div>
         <hr>
     </div>
-    <!-- /.col -->
-    <div class="col-md-12 end-box ">
-        &copy; 2014 | &nbsp; All Rights Reserved | &nbsp; www.yourdomain.com | &nbsp; 24x7 support | &nbsp; Email us: info@yourdomain.com
-    </div>
     </body>
 @endsection
 
